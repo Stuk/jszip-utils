702542642ebc57d6e00f948e56bd481912c8352b
Victor <3372598+victornpb@users.noreply.github.com>
2019-05-25 06:39:07 -0300

    build dist folder

--- dist/jszip-utils.js
+++ dist/jszip-utils.js
@@ -1,4 +1,4 @@
-/*!
+/*@preserve
 
 JSZipUtils - A collection of cross-browser utilities to go along with JSZip.
 <http://stuk.github.io/jszip-utils>
@@ -49,18 +49,33 @@ var createXHR = (typeof window !== "undefined" && window.ActiveXObject) ?
 
 
 
-JSZipUtils.getBinaryContent = function(path, cbo) {
-    //old callback style
-    if(typeof cbo==="function"){
-        var callback = cbo;
-        cbo = {
-            done: function(data){ callback(null, data); },
-            fail: function(err){ callback(err, null); }
-        };
+JSZipUtils.getBinaryContent = function (path, options) {
+
+    var promise, resolve, reject;
+    var callback;
+
+    // backward compatible callback
+    if (typeof options === "function") {
+        callback = options;
+        options = {};
+    }
+    // callback inside options object
+    else if (typeof options === 'object' && options && typeof options.callback === 'function') {
+        callback = options.callback;
+    }
+
+    if (!callback) {
+        promise = new window.Promise(function (_resolve, _reject) {
+            resolve = _resolve;
+            reject = _reject;
+        });
+    }
+    else {
+        resolve = function (data) { callback(null, data); };
+        reject = function (err) { callback(err, null); };
     }
     
-    if(typeof cbo!=="object" || typeof cbo.done==="function" || typeof cbo.fail==="function") throw new Error("You must specify a callback.done and callback.fail");
-    
+    if (!options) options = {};
     /*
      * Here is the tricky part : getting the data.
      * In firefox/chrome/opera/... setting the mimeType to 'text/plain; charset=x-user-defined'
@@ -100,36 +115,38 @@ JSZipUtils.getBinaryContent = function(path, cbo) {
                     err = null;
                     try {
                         file = JSZipUtils._getBinaryFromXHR(xhr);
-                        cbo.done(file);
+                        resolve(file);
                     } catch(rr) {
                         err = new Error(rr);
-                        cbo.fail(err);
+                        reject(err);
                     }
                 } else {
-                    cbo.fail(new Error("Ajax error for " + path + " : " + this.status + " " + this.statusText));
+                    reject(new Error("Ajax error for " + path + " : " + this.status + " " + this.statusText));
                 }
             }
         };
         
-        if(cbo.progress) {
+        if(options.progress) {
             xhr.onprogress = function(e) {
-                cbo.onprogress.call(xhr, {
+                options.progress({
                     path: path,
                     originalEvent: e,
                     percent: e.loaded / e.total * 100,
                     downloaded: e.loaded,
-                    size: e.total
+                    size: e.total,
+                    loaded: e.loaded,
+                    total: e.total
                 });
             };
         }
 
         xhr.send();
-        
-        return xhr;
 
     } catch (e) {
-        cbo.fail(new Error(e), null);
+        reject(new Error(e), null);
     }
+
+    return promise; // returns a promise or undefined if provided callback
 };
 
 // export
