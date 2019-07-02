3b6c8f9548b047097fb3d18d99f275a1c90211e9
Stuart Knightley <stuart@stuartk.com>
2019-07-02 15:13:13 -0700

    Guard Promise tests for IE

--- test/test.js
+++ test/test.js
@@ -95,128 +95,132 @@ QUnit.test("JSZipUtils.getBinaryContent, 404 NOT FOUND", function (assert) {
     assert.strictEqual(p, undefined, 'not return promise');
 });
 
-QUnit.module("Promise (no parameters)");
+// Guard Promise tests for IE
+if (typeof Promise === "undefined") {
+    QUnit.module("Promises");
+    QUnit.skip("Skipping promise tests");
+} else {
+    QUnit.module("Promise (no parameters)");
 
-QUnit.test("JSZipUtils.getBinaryContent amount, text, 200 OK", function (assert) {
-    var done = assert.async();
-    JSZipUtils.getBinaryContent("ref/amount.txt").then(function (data) {
-            assert.equal(toString(data), "\xe2\x82\xac\x31\x35\x0a", "The content has been fetched");
+    QUnit.test("JSZipUtils.getBinaryContent amount, text, 200 OK", function (assert) {
+        var done = assert.async();
+        JSZipUtils.getBinaryContent("ref/amount.txt").then(function (data) {
+                assert.equal(toString(data), "\xe2\x82\xac\x31\x35\x0a", "The content has been fetched");
+                done();
+            })
+            .catch(function (err) {
+                assert.equal(err, null, "no error");
+                done();
+            });
+    });
+
+    QUnit.test("JSZipUtils.getBinaryContent smile, image, 200 OK", function (assert) {
+        var done = assert.async();
+        JSZipUtils.getBinaryContent("ref/smile.gif").then(function (data) {
+            assert.equal(toString(data).indexOf("\x47\x49\x46\x38\x37\x61"), 0, "The content has been fetched");
             done();
-        })
-        .catch(function (err) {
+        }).catch(function (err) {
             assert.equal(err, null, "no error");
             done();
         });
-});
-
-QUnit.test("JSZipUtils.getBinaryContent smile, image, 200 OK", function (assert) {
-    var done = assert.async();
-    JSZipUtils.getBinaryContent("ref/smile.gif").then(function (data) {
-        assert.equal(toString(data).indexOf("\x47\x49\x46\x38\x37\x61"), 0, "The content has been fetched");
-        done();
-    }).catch(function (err) {
-        assert.equal(err, null, "no error");
-        done();
     });
-});
 
-QUnit.test("JSZipUtils.getBinaryContent nothing, 404 NOT FOUND", function (assert) {
-    var done = assert.async();
-    JSZipUtils.getBinaryContent("ref/nothing").then(function (data) {
-        assert.equal(data, null, "no error");
-        done();
-    }).catch(function (err) {
-        assert.ok(err instanceof Error, "The error is an Error");
-        done();
-    });
-});
-
-QUnit.module("Promise, options={}");
-
-QUnit.test("JSZipUtils.getBinaryContent amount, text, 200 OK", function (assert) {
-    var done = assert.async();
-    JSZipUtils.getBinaryContent("ref/amount.txt", {}).then(function (data) {
-            assert.equal(toString(data), "\xe2\x82\xac\x31\x35\x0a", "The content has been fetched");
+    QUnit.test("JSZipUtils.getBinaryContent nothing, 404 NOT FOUND", function (assert) {
+        var done = assert.async();
+        JSZipUtils.getBinaryContent("ref/nothing").then(function (data) {
+            assert.equal(data, null, "no error");
             done();
-        })
-        .catch(function (err) {
+        }).catch(function (err) {
+            assert.ok(err instanceof Error, "The error is an Error");
+            done();
+        });
+    });
+
+    QUnit.module("Promise, options={}");
+
+    QUnit.test("JSZipUtils.getBinaryContent amount, text, 200 OK", function (assert) {
+        var done = assert.async();
+        JSZipUtils.getBinaryContent("ref/amount.txt", {}).then(function (data) {
+                assert.equal(toString(data), "\xe2\x82\xac\x31\x35\x0a", "The content has been fetched");
+                done();
+            })
+            .catch(function (err) {
+                assert.equal(err, null, "no error");
+                done();
+            });
+    });
+
+    QUnit.test("JSZipUtils.getBinaryContent smile, image, 200 OK", function (assert) {
+        var done = assert.async();
+        JSZipUtils.getBinaryContent("ref/smile.gif", {}).then(function (data) {
+            assert.equal(toString(data).indexOf("\x47\x49\x46\x38\x37\x61"), 0, "The content has been fetched");
+            done();
+        }).catch(function (err) {
             assert.equal(err, null, "no error");
             done();
         });
-});
-
-QUnit.test("JSZipUtils.getBinaryContent smile, image, 200 OK", function (assert) {
-    var done = assert.async();
-    JSZipUtils.getBinaryContent("ref/smile.gif", {}).then(function (data) {
-        assert.equal(toString(data).indexOf("\x47\x49\x46\x38\x37\x61"), 0, "The content has been fetched");
-        done();
-    }).catch(function (err) {
-        assert.equal(err, null, "no error");
-        done();
     });
-});
 
-QUnit.test("JSZipUtils.getBinaryContent nothing, 404 NOT FOUND", function (assert) {
-    var done = assert.async();
-    JSZipUtils.getBinaryContent("ref/nothing", {}).then(function (data) {
-        assert.equal(data, null, "no error");
-        done();
-    }).catch(function (err) {
-        assert.ok(err instanceof Error, "The error is an Error");
-        done();
-    });
-});
-
-QUnit.module("Promise, options={progress}");
-
-QUnit.test("JSZipUtils.getBinaryContent amount, text, 200 OK", function (assert) {
-    var done = assert.async();
-    var progress = assert.async();
-    JSZipUtils.getBinaryContent("ref/amount.txt", { progress: function(e){
-        assert.ok(true, 'progress to be called');
-        assert.strictEqual(e.total, 6, 'total');
-        progress();
-    }}).then(function (data) {
-            assert.equal(toString(data), "\xe2\x82\xac\x31\x35\x0a", "The content has been fetched");
+    QUnit.test("JSZipUtils.getBinaryContent nothing, 404 NOT FOUND", function (assert) {
+        var done = assert.async();
+        JSZipUtils.getBinaryContent("ref/nothing", {}).then(function (data) {
+            assert.equal(data, null, "no error");
             done();
-        })
-        .catch(function (err) {
+        }).catch(function (err) {
+            assert.ok(err instanceof Error, "The error is an Error");
+            done();
+        });
+    });
+
+    QUnit.module("Promise, options={progress}");
+
+    QUnit.test("JSZipUtils.getBinaryContent amount, text, 200 OK", function (assert) {
+        var done = assert.async();
+        var progress = assert.async();
+        JSZipUtils.getBinaryContent("ref/amount.txt", { progress: function(e){
+            assert.ok(true, 'progress to be called');
+            assert.strictEqual(e.total, 6, 'total');
+            progress();
+        }}).then(function (data) {
+                assert.equal(toString(data), "\xe2\x82\xac\x31\x35\x0a", "The content has been fetched");
+                done();
+            })
+            .catch(function (err) {
+                assert.equal(err, null, "no error");
+                done();
+            });
+    });
+
+    QUnit.test("JSZipUtils.getBinaryContent smile, image, 200 OK", function (assert) {
+        var done = assert.async();
+        var progress = assert.async();
+        JSZipUtils.getBinaryContent("ref/smile.gif", { progress: function(e){
+            assert.ok(true, 'progress to be called');
+            assert.strictEqual(e.total, 41, 'total');
+            progress();
+        }}).then(function (data) {
+            assert.equal(toString(data).indexOf("\x47\x49\x46\x38\x37\x61"), 0, "The content has been fetched");
+            done();
+        }).catch(function (err) {
             assert.equal(err, null, "no error");
             done();
         });
-});
-
-QUnit.test("JSZipUtils.getBinaryContent smile, image, 200 OK", function (assert) {
-    var done = assert.async();
-    var progress = assert.async();
-    JSZipUtils.getBinaryContent("ref/smile.gif", { progress: function(e){
-        assert.ok(true, 'progress to be called');
-        assert.strictEqual(e.total, 41, 'total');
-        progress();
-    }}).then(function (data) {
-        assert.equal(toString(data).indexOf("\x47\x49\x46\x38\x37\x61"), 0, "The content has been fetched");
-        done();
-    }).catch(function (err) {
-        assert.equal(err, null, "no error");
-        done();
     });
-});
 
-QUnit.test("JSZipUtils.getBinaryContent nothing, 404 NOT FOUND", function (assert) {
-    var done = assert.async();
-    
-    JSZipUtils.getBinaryContent("ref/nothing", { progress: function(e){
-        
-    }}).then(function (data) {
-        assert.equal(data, null, "no error");
-        done();
-    }).catch(function (err) {
-        assert.ok(err instanceof Error, "The error is an Error");
-        done();
+    QUnit.test("JSZipUtils.getBinaryContent nothing, 404 NOT FOUND", function (assert) {
+        var done = assert.async();
+
+        JSZipUtils.getBinaryContent("ref/nothing", { progress: function(e){
+
+        }}).then(function (data) {
+            assert.equal(data, null, "no error");
+            done();
+        }).catch(function (err) {
+            assert.ok(err instanceof Error, "The error is an Error");
+            done();
+        });
     });
-});
-
-
+} // Promise tests
 
 // enforcing Stuk's coding style
 // vim: set shiftwidth=4 softtabstop=4:
